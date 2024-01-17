6bd398cb7a51a933ecc81d385f3df7f6ff1f22de
SuperJavierWorld <zacharysanford@outlook.com>
2024-01-17 16:46:09 -0500

    UI
    Made button code for UI

--- Unity Collab/Assets/Scripts/Floating.cs
+++ Unity Collab/Assets/Scripts/Floating.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Floating : MonoBehaviour
 {
     private bool isFloating = false;
     private float timer = 0;
     private float originalPos = 0;
-
-
+    public UnityEvent buttonClick;
+    public GameObject b;
+    public GameObject screen;
 
     void Start()
     {
@@ -17,28 +19,36 @@ public class Floating : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+        buttonClick.Invoke();
+        Debug.Log("Hallo");
+        b.SetActive(false);
+        screen.SetActive(false);
+        }   
+
         if (isFloating == false)
         {
-            timer += .0005f;
+            timer += .05f;
             transform.position = new Vector2(transform.position.x,timer+originalPos);
-            if (transform.position.y > .2+originalPos)
+            if (transform.position.y > 20+originalPos)
             {
-                timer -= .0003f;
+                timer -= .025f;
             }
-            if (transform.position.y > .25+originalPos)
+            if (transform.position.y > 25+originalPos)
             {
                 isFloating = true;
             }
         }
         else
         {
-            timer -= .0005f;
+            timer -= .05f;
             transform.position = new Vector2(transform.position.x,timer+originalPos);
-            if (transform.position.y < -.2+originalPos)
+            if (transform.position.y < -20+originalPos)
             {
-                timer += .0003f;
+                timer += .025f;
             }
-            if (transform.position.y < -.25+originalPos)
+            if (transform.position.y < -25+originalPos)
             {
                 isFloating = false;
             }
