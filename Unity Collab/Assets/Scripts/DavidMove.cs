dd99cb548ee7ae1b029ac42de402a1927731d164
SuperJavierWorld <zacharysanford@outlook.com>
2024-01-20 08:49:29 -0500

    Position
    Fixed the position of Davids

--- Unity Collab/Assets/Scripts/DavidMove.cs
+++ Unity Collab/Assets/Scripts/DavidMove.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Davidmove : MonoBehaviour
 {
+    void Start()
+    {
+        transform.position = new Vector2(12,-1);
+    }
+
     void Update()
     {
         transform.position = new Vector2(transform.position.x-.002f,transform.position.y);
